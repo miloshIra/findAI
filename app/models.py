87fabf1d6f05347586b34424890949b3c8e21d8e
miloshIra <maritonski@gmail.com>
2023-03-01 22:24:52 +0100

    steady wins the race

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
+import datetime
 
 
 class User:
-    def __init__(self, username, password):
+    def __init__(self, username, password, company):
         self.username = username
         self.password = password
+        self.company = company
+        self.created = datetime.datetime.now()
 
     def __repr__(self):
-        return self.username
+        return self
 
     def __str__(self):
         return self.username
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,32 @@
 from app import app
 from .models import User
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Fideri"
+    user = {"username": "Ira"}
+    services = [
+        {
+            'author': {'username': 'Ira'},
+            'description': 'AI model that sorts photos.'
+        },
+        {
+            'author': {'username': 'Stap'},
+            'description': 'AI that improves photo quality.'
+        }]
+    return render_template('index.html', title='Home', user=user, services=services)
 
 
-@app.route('/plebs/')
-def plebs():
-    return 'ti si fider!'
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user {}, remember me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/users/', methods=['GET'])
@@ -18,4 +34,5 @@ def get_users():
     user = User(username='Oliver', password='feeding')
     result = {"username": user.username,
               "password": user.password}
+
     return result
