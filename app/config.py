586ad74a9ea739da3451e2dc8663b832b4c8cfac
miloshIra <maritonski@gmail.com>
2023-03-07 22:11:19 +0100

    fixed same usename exception

--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_USL') or 'postgresql://postgres:postgres@localhost/findAIdb'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    os.environ['FLASK_DEBUG'] = 'True'
